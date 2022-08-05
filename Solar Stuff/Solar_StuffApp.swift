755a01c3ddeaf2d713b868915beecb3ed19ac9cd
TSanni <tomas.sanni@gmail.com>
2022-08-04 23:45:43 -0500

    Initial Commit

---
+++ Solar Stuff/Solar_StuffApp.swift
@@ -0,0 +1,17 @@
+//
+//  Solar_StuffApp.swift
+//  Solar Stuff
+//
+//  Created by Tomas Sanni on 8/4/22.
+//
+
+import SwiftUI
+
+@main
+struct Solar_StuffApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
